ea45a132d3ea47e43acf3fa14dcb2e9c575da08f
Taeseob <bayuafrizal830@gmail.com>
2025-08-24 10:00:06 +0700

    minor changing csrf

--- hr/hr/settings.py
+++ hr/hr/settings.py
@@ -153,3 +153,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = CSRF_SERVICE
